ac86df1d0979e21727ef7deda1df9857b938f05b
sanji950 <yu_dai26@yahoo.com.tw>
2022-07-27 16:45:37 +0800

    upload new components

---
+++ src/components/TweetCreateNew.vue
@@ -0,0 +1,100 @@
+<template>
+  <section>
+    <header class="header">
+      <h4 class="title">首頁</h4>
+    </header>
+    <form class="tweet-creat-from">
+      <div class="avatar">
+        <img src="./../assets/img/tweet-nophoto.png" alt="" />
+      </div>
+
+      <div class="twitter-text-container">
+        <textarea
+          class="twitter-text"
+          placeholder="有什麼新鮮事？"
+          maxlength="200"
+          v-model="twitterText"
+        ></textarea>
+      </div>
+      <button
+        class="btn btn-post-reply-tweet"
+        @click.stop.prevent="createdTweet"
+      >
+        推文
+      </button>
+    </form>
+  </section>
+</template>
+
+<script>
+import { Toast } from '../utils/helpers'
+
+export default {
+  data () {
+    return {
+      twitterText: ''
+    }
+  },
+  methods: {
+    createdTweet () {
+      if (!this.twitterText.trim()) {
+        Toast.fire({
+          icon: 'warning',
+          title: '推文內容不可空白'
+        })
+        return
+      }
+
+      if (this.twitterText.length > 140) {
+        Toast.fire({
+          icon: 'warning',
+          title: '推文字數不可超過 140 字'
+        })
+        return
+      }
+
+      Toast.fire({
+        icon: 'success',
+        title: '新增推文成功'
+      })
+
+      this.twitterText = ''
+
+      // todo: 串接
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tweet-creat-from {
+  display: grid;
+  grid-template-columns: 50px auto 10%;
+  grid-gap: 10px;
+  padding: 15px 20px;
+  width: 100%;
+  height: 160px;
+  border-bottom: 10px solid var(--border);
+  position: relative;
+}
+
+.twitter-text {
+  padding: 10px 5px;
+  height: 90%;
+  width: 100%;
+  border: none;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 26px;
+}
+
+.twitter-text::-webkit-scrollbar {
+  display: none;
+}
+
+.btn-post-reply-tweet {
+  position: absolute;
+  right: 20px;
+  bottom: 15px;
+}
+</style>
---
+++ src/components/TweetPopularList.vue
@@ -0,0 +1,286 @@
+<template>
+  <section class="tweet-popular-section">
+    <div class="tweet-popular-container">
+      <div class="tweet">
+        <div class="avatar">
+          <img src="./../assets/img/tweet-nophoto.png" alt="" />
+        </div>
+        <div class="tweet-info">
+          <div class="tweet-user">
+            <span class="tweet-user name">Apple</span>
+            <span class="tweet-user account">@apple ・3 小時</span>
+          </div>
+          <p class="tweet-content">
+            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
+            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
+            reprehenderit elit laborum.
+          </p>
+          <div class="tweet-icon-container">
+            <div class="tweet-reply">
+              <ModalTweetReply class="tweet-icon-reply" />
+              <span class="tweet-icon-number">13</span>
+            </div>
+
+            <div class="tweet-like">
+              <img
+                class="tweet-icon-like"
+                src="../assets/icon/tweet-like.svg"
+                alt=""
+              />
+              <span class="tweet-icon-number">13</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="tweet">
+        <div class="avatar">
+          <img src="./../assets/img/tweet-nophoto.png" alt="" />
+        </div>
+        <div class="tweet-info">
+          <div class="tweet-user">
+            <span class="tweet-user name">Apple</span>
+            <span class="tweet-user account">@apple ・3 小時</span>
+          </div>
+          <p class="tweet-content">
+            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
+            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
+            reprehenderit elit laborum.
+          </p>
+          <div class="tweet-icon-container">
+            <div class="tweet-reply">
+              <ModalTweetReply class="tweet-icon-reply" />
+              <span class="tweet-icon-number">13</span>
+            </div>
+
+            <div class="tweet-like">
+              <img
+                class="tweet-icon-like"
+                src="../assets/icon/tweet-like.svg"
+                alt=""
+              />
+              <span class="tweet-icon-number">13</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="tweet">
+        <div class="avatar">
+          <img src="./../assets/img/tweet-nophoto.png" alt="" />
+        </div>
+        <div class="tweet-info">
+          <div class="tweet-user">
+            <span class="tweet-user name">Sunny</span>
+            <span class="tweet-user account">@Sunny ・3 小時</span>
+          </div>
+          <p class="tweet-content">切版囉</p>
+          <div class="tweet-icon-container">
+            <div class="tweet-reply">
+              <ModalTweetReply class="tweet-icon-reply" />
+              <span class="tweet-icon-number">13</span>
+            </div>
+
+            <div class="tweet-like">
+              <img
+                class="tweet-icon-like"
+                src="../assets/icon/tweet-like.svg"
+                alt=""
+              />
+              <span class="tweet-icon-number">13</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="tweet">
+        <div class="avatar">
+          <img src="./../assets/img/tweet-nophoto.png" alt="" />
+        </div>
+        <div class="tweet-info">
+          <div class="tweet-user">
+            <span class="tweet-user name">Sunny</span>
+            <span class="tweet-user account">@Sunny ・3 小時</span>
+          </div>
+          <p class="tweet-content">切版囉</p>
+          <div class="tweet-icon-container">
+            <div class="tweet-reply">
+              <ModalTweetReply class="tweet-icon-reply" />
+              <span class="tweet-icon-number">13</span>
+            </div>
+
+            <div class="tweet-like">
+              <img
+                class="tweet-icon-like"
+                src="../assets/icon/tweet-like.svg"
+                alt=""
+              />
+              <span class="tweet-icon-number">13</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="tweet">
+        <div class="avatar">
+          <img src="./../assets/img/tweet-nophoto.png" alt="" />
+        </div>
+        <div class="tweet-info">
+          <div class="tweet-user">
+            <span class="tweet-user name">Sunny</span>
+            <span class="tweet-user account">@Sunny ・3 小時</span>
+          </div>
+          <p class="tweet-content">切版囉</p>
+          <div class="tweet-icon-container">
+            <div class="tweet-reply">
+              <ModalTweetReply class="tweet-icon-reply" />
+              <span class="tweet-icon-number">13</span>
+            </div>
+
+            <div class="tweet-like">
+              <img
+                class="tweet-icon-like"
+                src="../assets/icon/tweet-like.svg"
+                alt=""
+              />
+              <span class="tweet-icon-number">13</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="tweet">
+        <div class="avatar">
+          <img src="./../assets/img/tweet-nophoto.png" alt="" />
+        </div>
+        <div class="tweet-info">
+          <div class="tweet-user">
+            <span class="tweet-user name">Sunny</span>
+            <span class="tweet-user account">@Sunny ・3 小時</span>
+          </div>
+          <p class="tweet-content">切版囉</p>
+          <div class="tweet-icon-container">
+            <div class="tweet-reply">
+              <ModalTweetReply class="tweet-icon-reply" />
+              <span class="tweet-icon-number">13</span>
+            </div>
+
+            <div class="tweet-like">
+              <img
+                class="tweet-icon-like"
+                src="../assets/icon/tweet-like.svg"
+                alt=""
+              />
+              <span class="tweet-icon-number">13</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="tweet">
+        <div class="avatar">
+          <img src="./../assets/img/tweet-nophoto.png" alt="" />
+        </div>
+        <div class="tweet-info">
+          <div class="tweet-user">
+            <span class="tweet-user name">Sunny</span>
+            <span class="tweet-user account">@Sunny ・3 小時</span>
+          </div>
+          <p class="tweet-content">切版囉</p>
+          <div class="tweet-icon-container">
+            <div class="tweet-reply">
+              <ModalTweetReply class="tweet-icon-reply" />
+              <span class="tweet-icon-number">13</span>
+            </div>
+
+            <div class="tweet-like">
+              <img
+                class="tweet-icon-like"
+                src="../assets/icon/tweet-like.svg"
+                alt=""
+              />
+              <span class="tweet-icon-number">13</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ModalTweetReply from './ModalTweetReply.vue'
+
+export default {
+  components: {
+    ModalTweetReply
+  }
+}
+</script>
+
+<style scoped>
+.tweet-popular-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.tweet {
+  display: grid;
+  grid-template-columns: 50px auto;
+  grid-gap: 15px;
+  width: 100%;
+  height: auto;
+  padding: 10px 20px;
+  border-bottom: 1px solid var(--border);
+}
+
+.tweet-info {
+  display: grid;
+}
+
+.tweet-content {
+  margin: 5px 0;
+  line-height: 24px;
+  color: var(--dark-100);
+  font-weight: 400;
+}
+
+.tweet-user.name {
+  font-weight: 700;
+}
+
+.tweet-user.account {
+  margin: 0 10px;
+  font-size: 14px;
+  color: var(--secondary);
+}
+
+.tweet-icon-container {
+  display: flex;
+  width: 100%;
+}
+
+.tweet-reply,
+.tweet-like {
+  display: flex;
+  align-items: center;
+  margin-right: 42px;
+}
+
+.tweet-icon-like {
+  height: 16px;
+  width: 16px;
+}
+
+.tweet-icon-reply {
+  margin-top: 3px;
+  margin-right: 3px;
+
+}
+
+.tweet-icon-like {
+  margin-right: 12px;
+}
+</style>
